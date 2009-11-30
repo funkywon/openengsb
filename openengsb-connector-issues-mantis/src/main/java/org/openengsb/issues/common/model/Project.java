494bf32f47145d6ecd9e25f197e8ec200cc21835
Gi-Won Chung <giwonchung@gmail.com>
2009-11-30 19:41:52 +0100

    Changes in domain objects in package org.openengsb.issues.common.model. (From interfaces to classes)

--- openengsb-connector-issues-mantis/src/main/java/org/openengsb/issues/common/model/Project.java
+++ openengsb-connector-issues-mantis/src/main/java/org/openengsb/issues/common/model/Project.java
@@ -1,85 +1,116 @@
 package org.openengsb.issues.common.model;
 
-import java.io.File;
+import java.net.URI;
 import java.util.List;
 
 import org.openengsb.issues.common.enums.ProjectStatus;
 
-public interface Project {
+public class Project {
+	
+	private String id;
+    private String name;
+    private ProjectStatus status;
+    private String description;
+    private URI uri;
+    private List<Issue> issues;
 	
 	/**
 	 * Sets the id of the project to identify it.
 	 * @param id - the id identifies the project
 	 */
-	public void setId(Long id);
+	public void setId(String id) {
+		this.id=id;		
+	}
 	
 	/**
 	 * Supplies the identifier of the project
 	 * @return id - identifier of the project
 	 */
-	public Long getId();
+	public String getId() {
+		return this.id;
+	}
 	
 	/**
 	 * Sets the name of the project in the form of a short
 	 * @param name - short description
 	 */
-	public void setName(String name);
+	public void setName(String name) {
+		this.name=name;
+	}
 	
 	/**
 	 * Supplies the name of the project
 	 * @return name - short description of the project
 	 */
-	public String getName();
+	public String getName() {
+		return this.name;
+	}
 	
 	/**
 	 * Sets the status of the project
 	 * @param projectStatus - use the enumaration values for setting
 	 * @see ProjectStatus
 	 */
-	public void setStatus(ProjectStatus projectStatus);
+	public void setStatus(ProjectStatus projectStatus) {
+		this.status = projectStatus;
+	}
 	
 	/**
 	 * Supplies the topically status of the project
 	 * @return status - the topically status of the project
 	 * @see ProjectStatus
 	 */
-	public ProjectStatus getStatus();
+	public ProjectStatus getStatus() {
+		return this.status;
+	}
 	
 	/**
 	 * Sets the description of the project
 	 * @param description - a detailed description of the project
 	 */
-	public void setDescription(String description);
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 	/**
 	 * Supplies the detailed description of the project
 	 * @return description - in detailed form
 	 */
-	public String getDescription();
+	public String getDescription() {
+		return this.description;
+	}
 	
 	/**
-	 * Sets the URL of the project 
-	 * @param url - place on the file system(remote in most cases)
+	 * Sets the URI of the project 
+	 * @param uri - place on the file system(remote in most cases)
 	 */
-	public void setUrl(File url);
+	public void setUri(URI uri) {
+		this.uri = uri;
+	}
 	
 	/**
-	 * Supplies the URL of the project
-	 * @return url - place on the file system
+	 * Supplies the URI of the project
+	 * @return uri - place on the file system
 	 */
-	public File getUrl();
+	public URI getUri() {
+		return this.uri;
+	}
 	
 	/**
-	 * Sets the tasks for a project from the issue tracker
-	 * @param tasks - a list of tasks
+	 * Sets the issues for a project from the issue tracker
+	 * @param issues - a list of issues
 	 * @see Issue
 	 */
-	public void setIssues(List<Issue> tasks);
+	public void setIssues(List<Issue> issues) {
+		this.issues = issues;
+	}
 	
 	/**
 	 * Returns the tasks of the project
 	 * @return tasks - reported tasks of the project
 	 * @see Issue
 	 */
-	public List<Issue> getIssues();
+	public List<Issue> getIssues() {
+		return this.issues;
+	}
 }
