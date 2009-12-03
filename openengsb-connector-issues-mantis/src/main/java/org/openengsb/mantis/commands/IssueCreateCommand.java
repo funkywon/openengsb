ca86b064d7a8262d50bd9c2496b3ac1740620a93
Gi-Won Chung <giwonchung@gmail.com>
2009-12-03 21:24:04 +0100

    Corrected the errors. Added licence headers to Commands.

--- openengsb-connector-issues-mantis/src/main/java/org/openengsb/mantis/commands/IssueCreateCommand.java
+++ openengsb-connector-issues-mantis/src/main/java/org/openengsb/mantis/commands/IssueCreateCommand.java
@@ -1,4 +1,23 @@
+/**
+
+   Copyright 2009 OpenEngSB Division, Vienna University of Technology
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE\-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+ */
 package org.openengsb.mantis.commands;
+
+import javax.jbi.messaging.NormalizedMessage;
 
 import org.apache.commons.logging.Log;
 import org.openengsb.issues.common.api.IssueHandler;
@@ -15,7 +34,8 @@ public class IssueCreateCommand implements IssueCommand {
 	}
 	
 	@Override
-	public void execute() {
+	public String execute(NormalizedMessage in) {
+		return null;
 		// TODO Auto-generated method stub
 
 	}
