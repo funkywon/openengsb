8222ce4dc90198963c9d56113b45649ea452672b
Gi-Won Chung <giwonchung@gmail.com>
2009-12-28 23:14:48 +0100

    Beginning refactoring of IssuePriority enums

---
+++ openengsb-connector-issues-mantis/src/main/java/org/openengsb/issues/common/api/enums/priorities/Priority.java
@@ -0,0 +1,10 @@
+package org.openengsb.issues.common.api.enums.priorities;
+
+public abstract class Priority {
+	private static String value;
+	
+	public static String getValue() {
+		return value;
+	}
+
+}
