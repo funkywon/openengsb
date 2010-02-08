b109d3049441ec0d2c4824689513300bf13885d6
Gi-Won Chung <giwonchung@gmail.com>
2010-02-08 14:25:41 +0100

    Added test files

---
+++ openengsb-connector-issues-mantis/src/test/java/org/openengsb/mantis/MantisConnectorTest.java
@@ -0,0 +1,35 @@
+package org.openengsb.mantis;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnit44Runner;
+import org.openengsb.mantis.util.MantisTypeConverter;
+
+import biz.futureware.mantisconnect.MantisConnectPortType;
+
+@RunWith(MockitoJUnit44Runner.class)
+public class MantisConnectorTest {
+
+    private MantisIssueHandlerImpl mantisConnector;
+    @Mock private MantisTypeConverter typeConverter;
+    @Mock private MantisConnectPortType portType;
+    
+    @Before
+    public void setup() {
+        mantisConnector = new MantisIssueHandlerImpl("", "", "");
+        mantisConnector.setPorttype(portType);
+        mantisConnector.setTypeConverter(typeConverter);
+        
+        when
+    }
+    
+    @Test
+    public void createIssue() {
+        
+    }
+    
+    
+    
+    
+}
